40e9a2856083667ff058a5aed86831a7b9d1c04d
Omkar-M <malkaromkar22@gmail.com>
2021-09-03 16:34:01 +0530

    day 1 & 2

---
+++ day 1/3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int number1,number2;
+
+    cout << "Enter an two integer: ";
+    cin >> number1 >> number2;
+
+    cout << "Addition = " << number1 + number2;    
+    return 0;
+}
---
+++ day 2/2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    bool isVowel;
+    cout << "Enter an alphabet: ";
+    cin >> c;
+
+    isVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    // show error message if c is not an alphabet
+    if (!isalpha(c))
+      printf("Neither vowel nor consonant.");
+    else if (isVowel)
+        cout << c << " is a vowel.";
+    else
+        cout << c << " is a consonant.";
+    return 0;
+}
